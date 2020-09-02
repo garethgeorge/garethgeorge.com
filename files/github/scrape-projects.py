d64cdfd4ad0530399b0605093020b973e54e1c43
Gareth George <garethgeorge97@gmail.com>
2020-09-01 23:59:32 -0700

    added link href files

--- files/github/scrape-projects.py
+++ files/github/scrape-projects.py
@@ -18,11 +18,15 @@ for repo in g.get_user().get_repos():
             os.makedirs(os.path.join(dir, repo.full_name))
         except FileExistsError: 
             pass
+        
+        with open(os.path.join(dir, repo.full_name, "link.href"), "w") as f:
+            f.write(repo.html_url)
+
         try:
             resp = repo.get_contents("README.md")
             assert(resp.encoding == "base64")
             print("WRITING OUT README FILE")
-            with open(os.path.join(dir, repo.full_name, "README.md"), "w") as f:
+            with open(os.path.join(dir, repo.full_name, "readme.md"), "w") as f:
                 f.write(base64.b64decode(resp.content).decode("ascii"))
         except Exception as e:
             print(e)
